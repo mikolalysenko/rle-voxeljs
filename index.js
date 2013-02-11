bf0a64efe3cc323c5b988e13dd85d240256d1485
Mikola Lysenko <mikolalysenko@gmail.com>
2013-02-10 19:27:34 -0800

    basic implementation

---
+++ index.js
@@ -0,0 +1,13 @@
+var classify = require("rle-classify");
+
+//Converts a narrowband level set into a voxel.js volume
+module.exports = function(volume, voxels, bounds) {
+  var lo = bounds[0], hi = bounds[1];
+  for(var k=lo[2]; k<hi[2]; ++k) {
+    for(var j=lo[1]; j<hi[1]; ++j) {
+      for(var i=lo[0]; i<hi[0]; ++i) {
+        voxels.setBlock(i, j, k, classify.point(volume, [i,j,k]));
+      }
+    }
+  }
+}

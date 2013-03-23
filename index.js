a0612704ca7f8b70ac778b5886e31220483ea9f1
Mikola Lysenko <mikolalysenko@gmail.com>
2013-03-23 08:43:45 -0500

    removed extra allocation from inner loop

--- index.js
+++ index.js
@@ -2,21 +2,19 @@ var classify = require("rle-classify");
 
 //Converts a narrowband level set into a voxel.js volume
 function convertToVoxelJS(volume, voxels, bounds, startPos) {
-  var lo = bounds[0], hi = bounds[1];
+  var lo = bounds[0]
+    , hi = bounds[1]
+    , point = [0,0,0]
+    , shift = startPos || [0,0,0]
   for(var k=lo[2]; k<hi[2]; ++k) {
+    point[2] = k + shift[2]
     for(var j=lo[1]; j<hi[1]; ++j) {
+      point[1] = j + shift[1]
       for(var i=lo[0]; i<hi[0]; ++i) {
-        var point = [i, j, k];
+        point[0] = i + shift[0]
         var val = classify.point(volume, point);
         if (val === 0) {
           continue;
-        }
-        if (startPos) {
-          point = [
-            startPos[0] + point[0],
-            startPos[1] + point[1],
-            startPos[2] + point[2]
-          ];
         }
         voxels.setBlock(point, val);
       }
